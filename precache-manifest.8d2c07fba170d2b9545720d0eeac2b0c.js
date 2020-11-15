45ff461f03f8080efa42d4b8808ca137eda59f65
s00zn <susan@sheepwns.com>
2020-11-15 16:22:00 +0100

    Updates

---
+++ precache-manifest.8d2c07fba170d2b9545720d0eeac2b0c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e8356ef04482fc9a4a07120c82777c3",
+    "url": "/CatPals/index.html"
+  },
+  {
+    "revision": "cb6974a9d9de3a7de6c8",
+    "url": "/CatPals/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "6f2db3febd855a4b9ce9",
+    "url": "/CatPals/static/css/main.e2a014ca.chunk.css"
+  },
+  {
+    "revision": "cb6974a9d9de3a7de6c8",
+    "url": "/CatPals/static/js/2.07bbb8a4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CatPals/static/js/2.07bbb8a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f2db3febd855a4b9ce9",
+    "url": "/CatPals/static/js/main.fdea868a.chunk.js"
+  },
+  {
+    "revision": "0001769a0985dd0713d8",
+    "url": "/CatPals/static/js/runtime-main.1404dcf8.js"
+  }
+]);
